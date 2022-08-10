49d3fefa67787dee5c67ad6c23252506fb006b8e
Bhaumik V Patel <patelbha@pmc.local>
2022-08-10 12:37:04 +0530

    JSON Object Mapping Changes

--- src/test/java/org/example/JsonObjectTest.java
+++ src/test/java/org/example/JsonObjectTest.java
@@ -8,12 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class JsonObjectTest {
+
     static PojoJSONTest test;
     static FileInputStream fis;
     static JsonReader reader;
     static JsonObject  jsonObject;
     static final String path=System.getProperty("user.dir").concat("\\src\\main\\resources\\sampleJSON.json");
     static final FileWriter file;
+
+
     static {
         try {
             file = new FileWriter(System.getProperty("user.dir").concat("\\src\\main\\resources\\sampleJSONOutput.json"));
@@ -21,41 +24,34 @@ public class JsonObjectTest {
             throw new RuntimeException(e);
         }
     }
+
     static Address address;
-    static String firstname=jsonObject.getString("firstname");
-    static String lastname=jsonObject.getString("lastname");
-    static int age=jsonObject.getInt("age");
-    static JsonObject nestedAddressObject=jsonObject.getJsonObject("address");
-    static String streetaddress=nestedAddressObject.getString("streetaddress");
-    static String city=nestedAddressObject.getString("city");
-    static String state=nestedAddressObject.getString("state");
-    static String postalcode=nestedAddressObject.getString("postalcode");
-    static JsonArray array=jsonObject.getJsonArray("companylist");
+
     @Test
     public static PojoJSONTest getData() throws IOException {
         fis=new FileInputStream(path);
         reader= Json.createReader(fis);
         jsonObject= reader.readObject();
-
         //we can close IO resource and JsonReader now
         reader.close();
         fis.close();
-
         //Retrieve data from JsonObject and create pojo object
         test=new PojoJSONTest();
-        test.setFirstname(firstname);
-        test.setLastname(lastname);
-        test.setAge(age);
+        test.setFirstname(jsonObject.getString("firstname"));
+        test.setLastname(jsonObject.getString("lastname"));
+        test.setAge(jsonObject.getInt("age"));
 
         //reading inner object from json object
+        JsonObject nestedAddressObject=jsonObject.getJsonObject("address");
         address=new Address();
-        address.setStreetaddress(streetaddress);
-        address.setCity(city);
-        address.setState(state);
-        address.setPostalcode(postalcode);
+        address.setStreetaddress(nestedAddressObject.getString("streetaddress"));
+        address.setCity(nestedAddressObject.getString("city"));
+        address.setState(nestedAddressObject.getString("state"));
+        address.setPostalcode(nestedAddressObject.getString("postalcode"));
         test.setAddress(address);
 
         //reading arrays from json
+        JsonArray array=jsonObject.getJsonArray("companylist");
         String[] companylist=new String[array.size()];
         int i=0;
         for(JsonValue value:array)
@@ -67,18 +63,24 @@ public class JsonObjectTest {
     }
 
     @Test
-    public void readJSON() throws IOException {
+    public static void readJSON() throws IOException {
 
         test=getData();
         System.out.println(test);
     }
 
     @Test
-    public void writeJsonWithGson() throws IOException {
+    public static void writeJsonWithGson() throws IOException {
 
         test=getData();
         file.write(String.valueOf(new Gson().toJson(test)));
         file.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        readJSON();
+        writeJsonWithGson();
+
     }
 
 }
